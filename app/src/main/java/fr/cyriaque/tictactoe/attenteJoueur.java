edd2b643163cfe12c209afccdb77930e1520be2e
cecile.crochon <cecile.crochon@progress-it.fr>
2020-05-01 12:30:25 +0200

    feat:Vue

---
+++ app/src/main/java/fr/cyriaque/tictactoe/attenteJoueur.java
@@ -0,0 +1,14 @@
+package fr.cyriaque.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class attenteJoueur extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attente_joueur);
+    }
+}
